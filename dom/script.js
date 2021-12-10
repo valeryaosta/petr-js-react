965b9b1a68d56da55b7ba23988cae9b2a1c0e98c
valeryaosta <v.gerasimenko@nord-soft.com>
2021-12-10 11:46:21 +0300

    l - dom navigation, data-attr

---
+++ dom/script.js
@@ -0,0 +1,25 @@
+// console.log(document.body);
+// console.log(document.head);
+
+// console.log(document.documentElement);
+
+//console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+
+// console.log(document.querySelector('#current').parentNode.parentNode);
+
+//console.log(document.querySelector('[data-current = "3"]').nextSibling);
+//console.log(document.querySelector('[data-current = "3"]').previousSibling);
+
+console.log(document.querySelector('[data-current = "3"]').nextElementSibling);
+
+for (let node of document.body.childNodes) {
+    if(node.nodeName === '#text') {
+        continue;
+    }
+
+    console.log(node);
+}
+
+console.log(document.querySelectorAll(".wrapper"))
