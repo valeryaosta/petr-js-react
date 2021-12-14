bdceb6733330c86102bb5d5250c1564addd4670b
valeryaosta <gerasimenko.valery@gmail.com>
2021-12-14 11:07:01 +0300

    40 - Date

---
+++ 40/script.js
@@ -0,0 +1,32 @@
+// const now = new Date();
+// const now1 = new Date();
+// const now2 = new Date('2021-12-14');
+// const now3 = new Date(2021, 1, 7, 20, 15, 1);
+// const now4 = new Date(0);
+// const now5 = new Date(-555555);
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
+
+// const now = new Date();
+// console.log(now.setHours(10));
+// console.log(now.setHours(40));
+// console.log(now.setHours(10, 25, 20));
+// console.log(now);
+
+// const nowX = new Date('2021-12-14');
+// console.log(nowX);
+// console.log(new Date.parse('2021-12-14'));
+
+// let start = new Date();
+// for (let i=0; i < 100000; i++) {
+//     let some = i ** 3
+// }
+// let end = new Date();
+// alert(`Цикл отработал за ${end - start} миллисекунд`);
