b000d523087cd99524642274656944236a2cee86
valeryaosta <gerasimenko.valery@gmail.com>
2022-01-03 22:03:14 +0300

    78 - jquery

---
+++ 78/script.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+
+// DOMContentLoaded
+$(document).ready(function () {
+    $('.list-item:first').hover(function () {
+        $(this).toggleClass('active');
+    });
+
+    // addEventListener analog; hide element
+    $('.list-item:eq(2)').on('click', function () {
+        $('.image:even').fadeToggle('slow');
+    });
+
+    $('.list-item:eq(4)').on('click', function () {
+        $('.image:odd').animate({
+            opacity: 'toggle',
+            height: 'toggle'
+        }, 2000);
+    });
+
+});
