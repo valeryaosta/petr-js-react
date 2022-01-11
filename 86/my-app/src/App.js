2ce3049e7654834d7a931fa87c534e83be05bfc9
valeryaosta <v.gerasimenko@nord-soft.com>
2022-01-11 22:13:21 +0300

    96 - events & this

--- 86/my-app/src/App.js
+++ 86/my-app/src/App.js
@@ -6,25 +6,42 @@ class WhoAmI extends React.Component {
         super(props);
         this.state = {
             years: 27,
-            text: '+++'
+            position: ''
         }
+        // this.nextYear = this.nextYear.bind(this)
     }
 
     nextYear = () => {
-
+        // nextYear() {
         this.setState((state) => ({
             years: state.years + 1
         }))
+    }
+    commitInputChanges = (e, color) => {
+        //узнать у объекта события на каком элементе произошло событие
+        console.log(e.target.value)
+        console.log(color)
+        this.setState({
+            position: e.target.value
+        })
     }
 
 
     render() {
         const {name, surname, link} = this.props
+        const {position, years} = this.state
         return (
             <div>
-                <button onClick={this.nextYear}>{this.state.text}</button>
-                <h1>My name is {name}, surname - {surname}, age - {this.state.years} </h1>
+                {/*<button onClick={() => this.nextYear()}>+++</button>*/}
+                <button onClick={this.nextYear}>+++</button>
+                <h1>My name is {name}, surname - {surname}, age - {years},
+                    position - {position}
+                </h1>
                 <a href={link}>My Profile</a>
+                <form>
+                    <span>Type your position</span>
+                    <input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')}/>
+                </form>
             </div>
         )
     }
