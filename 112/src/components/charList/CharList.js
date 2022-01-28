1ae26581e6ad767ec45f60eef12a3e632db62fb7
valeryaosta <gerasimenko.valery@gmail.com>
2022-01-28 17:32:29 +0300

    116 - life-cycle methods practice + ComponentDidMount

--- 112/src/components/charList/CharList.js
+++ 112/src/components/charList/CharList.js
@@ -45,7 +45,9 @@ class CharList extends React.Component {
             return (
                 <li
                     className="char__item"
-                    key={item.id}>
+                    key={item.id}
+                    onClick={() => this.props.onCharSelected(item.id)}
+                >
                     <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                     <div className="char__name">{item.name}</div>
                 </li>
