390732908521e4af368fa8cf5c93a53af755b966
valeryaosta <gerasimenko.valery@gmail.com>
2022-02-08 21:57:44 +0300

    124 - refs

--- 112/src/components/charList/CharList.js
+++ 112/src/components/charList/CharList.js
@@ -3,6 +3,7 @@ import './charList.scss';
 import MarvelService from "../../services/MarvelService";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import Spinner from "../spinner/Spinner";
+
 // import PropTypes from 'prop-types';
 
 class CharList extends React.Component {
@@ -59,9 +60,21 @@ class CharList extends React.Component {
         })
     }
 
+    itemRefs = [];
+
+    setRef = (ref) => {
+        this.itemRefs.push(ref);
+    }
+
+    focusOnItem = (id) => {
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected');
+        this.itemRefs[id].focus();
+    }
+
     // метод создан для оптимизации, чтобы не помещать такую конструкцию в метод render
     renderItems(arr) {
-        const items = arr.map((item) => {
+        const items = arr.map((item, i) => {
             let imgStyle = {'objectFit': 'cover'};
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = {'objectFit': 'unset'};
@@ -71,8 +84,19 @@ class CharList extends React.Component {
                 <li
                     className="char__item"
                     key={item.id}
-                    onClick={() => this.props.onCharSelected(item.id)}
-                >
+                    tabIndex={0}
+                    ref={this.setRef}
+                    onClick={() => {
+                        this.props.onCharSelected(item.id);
+                        this.focusOnItem(i)
+                    }}
+                    onKeyPress={(e) => {
+                        if (e.key === ' ' || e.key === "Enter") {
+                            this.props.onCharSelected(item.id);
+                            this.focusOnItem(i);
+                        }
+                    }}>
+                    >
                     <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                     <div className="char__name">{item.name}</div>
                 </li>
---
+++ 86/my-app/src/App1.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import {Container} from 'react-bootstrap';
+import './App.css';
+
+class Form extends Component {
+    // myRef = React.createRef()
+
+    // componentDidMount() {
+    // this.myRef.current.focus()
+    // }
+
+    setInputRef = (elem) => {
+        this.myRef = elem
+    }
+
+    focusFirstTI = () => {
+        if (this.myRef) {
+            // *** если ипсользуем прием callback ref, тогда св-ва current здесь не будет!!!
+            this.myRef.focus()
+        }
+    }
+
+    render() {
+        return (
+            <Container>
+                <form className="w-50 border mt-5 p-3 m-auto">
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                        <input
+                            // ref={this.myRef}
+                            ref={this.setInputRef}
+                            type="email" className="form-control" id="exampleFormControlInput1"
+                            placeholder="name@example.com"/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                        <textarea onClick={this.focusFirstTI} className="form-control" id="exampleFormControlTextarea1"
+                                  rows="3"></textarea>
+                    </div>
+                </form>
+            </Container>
+        )
+    }
+}
+
+function App() {
+    return (
+        <Form/>
+    );
+}
+
+export default App;
