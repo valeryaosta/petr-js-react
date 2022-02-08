a976937dbf473d815042b1c5dd549b86853611b0
valeryaosta <gerasimenko.valery@gmail.com>
2022-02-08 22:24:29 +0300

    124 - react portals

--- 86/my-app/src/index.js
+++ 86/my-app/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import App1 from "./App1";
+// import App1 from "./App1";
+import App2 from "./App2";
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App1 />
+    <App2 />
   </React.StrictMode>,
   document.getElementById('root')
 );
