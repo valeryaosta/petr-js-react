ba88c3b31c3f9d9d2276b595c9563a6cad09b39e
valeryaosta <gerasimenko.valery@gmail.com>
2022-02-14 20:27:49 +0300

    133 - creating custom hook

--- 86/my-app/src/index.js
+++ 86/my-app/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 // import App1 from "./App1";
-import App2 from "./App2";
+// import App2 from "./App2";
+import App3 from "./App3";
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App2 />
+    <App3 />
   </React.StrictMode>,
   document.getElementById('root')
 );
