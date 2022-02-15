b05a58e0402ec806ab2cc4e17ed1f7ab40c081bb
valeryaosta <gerasimenko.valery@gmail.com>
2022-02-16 01:05:59 +0300

    136 - React Router v5

--- 112/src/components/app/App.js
+++ 112/src/components/app/App.js
@@ -1,40 +1,25 @@
-import React, {useState} from "react";
+import React from "react";
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import decoration from '../../resources/img/vision.png';
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import AppBanner from "../appBanner/AppBanner";
-import ComicsList from "../comicsList/ComicsList";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {MainPage, ComicsPage} from "../pages";
 
 const App = () => {
-    const [selectedChar, setSelectedChar] = useState(null)
-
-    const onCharSelected = (id) => {
-        setSelectedChar(id)
-    }
-
     return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-            {/*  <ErrorBoundary>
-                    <RandomChar/>
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected}/>
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/> */}
-                <AppBanner/>
-                <ComicsList />
-            </main>
-        </div>
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Switch>
+                        <Route exact path="/">
+                            <MainPage/>
+                        </Route>
+                        <Route exact path="/comics">
+                            <ComicsPage/>
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
+        </Router>
     )
 }
 
---
+++ 112/src/components/pages/ComicsPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AppBanner from "../appBanner/AppBanner";
+import ComicsList from "../comicsList/ComicsList";
+
+export const ComicsPage = () => {
+    return (
+        <>
+            <AppBanner/>
+            <ComicsList/>
+        </>
+    )
+}
