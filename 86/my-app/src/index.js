e831fdfecb0c55d76eb76ad0e1157500109c404e
valeryaosta <gerasimenko.valery@gmail.com>
2022-02-18 22:57:15 +0300

    141 - useReducer

--- 86/my-app/src/index.js
+++ 86/my-app/src/index.js
@@ -5,12 +5,13 @@ import './index.css';
 // import App1 from "./App1";
 // import App2 from "./App2";
 // import App3 from "./App3";
-import App4 from "./App4";
+// import App4 from "./App4";
+import App5 from "./App5";
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App4 />
+    <App5 />
   </React.StrictMode>,
   document.getElementById('root')
 );
