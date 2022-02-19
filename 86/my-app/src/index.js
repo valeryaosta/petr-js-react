7bb19daf85236ad1224ca235212a097772a8f2a3
valeryaosta <gerasimenko.valery@gmail.com>
2022-02-19 14:30:00 +0300

    142 - HOC

--- 86/my-app/src/index.js
+++ 86/my-app/src/index.js
@@ -6,12 +6,13 @@ import './index.css';
 // import App2 from "./App2";
 // import App3 from "./App3";
 // import App4 from "./App4";
-import App5 from "./App5";
+// import App5 from "./App5";
+import App6 from "./App6";
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App5 />
+    <App6 />
   </React.StrictMode>,
   document.getElementById('root')
 );
