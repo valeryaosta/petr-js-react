215ae2aabd857927ab9b21b8a49e9d1f4eb59889
valeryaosta <gerasimenko.valery@gmail.com>
2022-06-24 00:35:34 +0300

    196 - redux devtools connected

--- 190/src/index.js
+++ 190/src/index.js
@@ -7,7 +7,10 @@ import {Provider} from "react-redux";
 import {createStore} from "redux";
 import {reducer} from "./reducer/reducer";
 
-export const store = createStore(reducer)
+export const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
