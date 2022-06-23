b75259367cb876b76232606af516e7b69150b41d
valeryaosta <gerasimenko.valery@gmail.com>
2022-06-24 00:29:42 +0300

    195 - connected react-redux using hooks

--- 190/src/components/Counter.js
+++ 190/src/components/Counter.js
@@ -1,39 +1,30 @@
 import React from 'react';
-import {connect} from "react-redux";
-// import {dec, inc, rnd} from "../actions";
-// import {bindActionCreators} from "redux";
-import * as actions from "../actions";
+import {useDispatch, useSelector} from "react-redux";
+import {dec, inc, rnd} from "../actions";
 
 
-const Counter = ({counter, inc, dec, rnd}) => {
+const Counter = () => {
+    const counter = useSelector(state => state.counter)
+    const dispatch = useDispatch()
+
+    const decrease = () => {
+        dispatch(dec())
+    }
+    const increase = () => {
+        dispatch(inc())
+    }
+    const random = () => {
+        dispatch(rnd())
+    }
 
     return (
         <div className="jumbotron">
             <h1>{counter}</h1>
-            <button onClick={dec} className="btn btn-primary">DEC</button>
-            <button onClick={inc} className="btn btn-primary">INC</button>
-            <button onClick={rnd} className="btn btn-primary">RND</button>
+            <button onClick={decrease} className="btn btn-primary">DEC</button>
+            <button onClick={increase} className="btn btn-primary">INC</button>
+            <button onClick={random} className="btn btn-primary">RND</button>
         </div>
     );
 };
 
-const mapStateToProps = (state) => {
-    return {
-        counter: state.value
-    }
-}
-
-// const mapDispatchToProps = (dispatch) => {
-// // 1 вариант
-//     return {
-//     inc: () => dispatch(inc()),
-//     dec: () => dispatch(dec()),
-//     rnd: () => dispatch(rnd()),
-//     }
-// // 2 вариант
-//     return bindActionCreators(actions, dispatch)
-// }
-
-// либо передаем во второй объект все АС
-// export default connect(mapStateToProps, {inc, dec, rnd})(Counter);
-export default connect(mapStateToProps, actions)(Counter);
+export default Counter
--- 190/src/reducer/reducer.js
+++ 190/src/reducer/reducer.js
@@ -1,25 +1,25 @@
 
 const initState = {
-    value: 0
+    counter: 0
 }
 export const reducer = (state = initState, action) => {
     switch (action.type) {
         case 'INC' : {
             return {
                 ...state,
-                value: state.value + 1
+                counter: state.counter + 1
             }
         }
         case 'DEC' : {
             return {
                 ...state,
-                value: state.value - 1
+                counter: state.counter - 1
             }
         }
         case 'RND' : {
             return {
                 ...state,
-                value: state.value * action.payload
+                counter: state.counter * action.payload
             }
         }
 
