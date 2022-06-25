831f9c29e87d16530fb908a9d1f69b161d20d4de
valeryaosta <gerasimenko.valery@gmail.com>
2022-06-26 01:06:12 +0300

    199 - combineReducers, createSelector

--- 197/src/components/heroesList/HeroesList.js
+++ 197/src/components/heroesList/HeroesList.js
@@ -1,9 +1,9 @@
 import {useHttp} from '../../hooks/http.hook';
-import { useEffect, useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { CSSTransition, TransitionGroup} from 'react-transition-group';
-
-import { heroesFetching, heroesFetched, heroesFetchingError, heroDeleted } from '../../actions';
+import {useEffect, useCallback} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {createSelector} from 'reselect'
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
+import {heroesFetching, heroesFetched, heroesFetchingError, heroDeleted} from '../../actions';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
 
@@ -15,7 +15,22 @@ import './heroesList.scss';
 // Удаление идет и с json файла при помощи метода DELETE
 
 const HeroesList = () => {
-    const {filteredHeroes, heroesLoadingStatus} = useSelector(state => state);
+
+    const filteredHeroesSelector = createSelector(
+        (state) => state.filters.activeFilter,
+        (state) => state.heroes.heroes,
+        (filter, heroes) => {
+            if (filter === 'all') {
+                return heroes
+            } else {
+                return heroes.filter(item => item.element === filter)
+            }
+        }
+    )
+
+    const filteredHeroes = useSelector(filteredHeroesSelector);
+
+    const heroesLoadingStatus = useSelector(state => state);
     const dispatch = useDispatch();
     const {request} = useHttp();
 
--- 197/src/store/index.js
+++ 197/src/store/index.js
@@ -1,5 +1,13 @@
 import { createStore } from 'redux';
-import reducer from '../reducers';
+import {combineReducers} from "@reduxjs/toolkit";
+import heroes from "../reducers/heroes";
+import filters from "../reducers/filters";
+
+export const reducer = combineReducers({
+    heroes,
+    filters
+})
+
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
