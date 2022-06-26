f24d636a06fc377a75c0c82e4282b13e6c93b7c4
valeryaosta <gerasimenko.valery@gmail.com>
2022-06-26 21:47:07 +0300

    204 - rtk, configureStore

--- 197/src/store/index.js
+++ 197/src/store/index.js
@@ -1,13 +1,6 @@
-import {createStore, compose} from 'redux';
-import {applyMiddleware, combineReducers} from "@reduxjs/toolkit";
+import {configureStore} from "@reduxjs/toolkit";
 import heroes from "../reducers/heroes";
 import filters from "../reducers/filters";
-import thunk from "redux-thunk";
-
-export const reducer = combineReducers({
-    heroes,
-    filters
-})
 
 // next - dispatch, вместо next будет вызываться след ф-ция мз миддлевара
 const stringMiddleware = (store) => (next) => (action) => {
@@ -19,13 +12,20 @@ const stringMiddleware = (store) => (next) => (action) => {
     return next(action)
 }
 
-const store = createStore(
-    reducer,
-    compose(
-        applyMiddleware(thunk, stringMiddleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-);
+// const store = createStore(
+//     reducer,
+//     compose(
+//         applyMiddleware(thunk, stringMiddleware),
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     )
+// );
+
+const store = configureStore({
+    reducer: {heroes, filters},
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
+    devTools: process.env.NODE_ENV !== 'production'
+})
+
 
 export default store;
 
