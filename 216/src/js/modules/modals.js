f68489ae3692cfe89c6b45de745522e572d85165
valeryaosta <gerasimenko.valery@gmail.com>
2022-07-08 01:01:00 +0300

    220- form-calculator part 1

--- 216/src/js/modules/modals.js
+++ 216/src/js/modules/modals.js
@@ -1,15 +1,20 @@
 const modals = () => {
-    function bindModal(triggerSelector, modalSelector, closeSelector) {
+    function bindModal(triggerSelector, modalSelector, closeSelector, closeCLickOverlay = true) {
 
         const trigger = document.querySelectorAll(triggerSelector);
         const modal = document.querySelector(modalSelector);
         const close = document.querySelector(closeSelector);
+        const windows = document.querySelectorAll('[data-modal]');
 
         trigger.forEach(item => {
             item.addEventListener('click', (e) => {
                 if (e.target) {
                     e.preventDefault();
                 }
+
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                })
 
                 modal.style.display = 'block';
                 document.body.style.overflow = 'hidden';
@@ -19,13 +24,21 @@ const modals = () => {
         })
 
         close.addEventListener('click', () => {
+            windows.forEach(item => {
+                item.style.display = 'none';
+            })
+
             modal.style.display = 'none';
             document.body.style.overflow = '';
             // document.body.classList.remove('modal-open');
         })
 
         modal.addEventListener('click', (e) => {
-            if (e.target === modal) {
+            if (e.target === modal && closeCLickOverlay) {
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                })
+
                 modal.style.display = 'none';
                 document.body.style.overflow = '';
                 // document.body.classList.remove('modal-open');
@@ -42,6 +55,9 @@ const modals = () => {
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
+    bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+    bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
+    bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
 
     // showModalByTime('.popup', 60000);
 
