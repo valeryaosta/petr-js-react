3b5f50b427b5779792c462fc253a4901e8f2f9ef
valeryaosta <gerasimenko.valery@gmail.com>
2022-07-08 15:30:12 +0300

    222- timer

---
+++ 216/src/js/modules/timer.js
@@ -0,0 +1,64 @@
+const timer = (id, deadline) => {
+
+    const addZero = (num) => {
+        if (num <= 9) {
+            return '0' + num;
+        } else {
+            return num
+        }
+    }
+
+    const getTimeRemaining = (endTime) => {
+        const time = Date.parse(endTime) - Date.parse(new Date());
+        // все количество времени в секундах
+        const seconds = Math.floor((time / 1000) % 60);
+        // все количество времени в минутах
+        const minutes = Math.floor((time / 1000 / 60) % 60);
+        // общее количество часов во времени
+        const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+        // общее количество дней
+        const days = Math.floor((time / (1000 * 60 * 60 * 24)));
+
+        return {
+            'total': time,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        }
+    }
+
+    const setCLock = (selector, endTime) => {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000)
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endTime);
+
+            days.textContent = addZero(t.days);
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                days.textContent = '00';
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setCLock(id, deadline);
+
+};
+
+export default timer;
